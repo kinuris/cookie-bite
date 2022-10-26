a15a30e1e3f8735d5bb6993ab0063374ad4f75a0
Chris Dominic Chan <chanchrisdominic@gmail.com>
2022-10-26 13:52:29 +0800

    added dropdown to menu

--- src/root-components/Sidebar.tsx
+++ src/root-components/Sidebar.tsx
@@ -1,7 +1,7 @@
 /**@jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { background, foreground, textcolor } from "../Colors";
-import { motion } from "framer-motion"
+import { background, backgroundMenuItem, foreground, textcolor } from "../Colors";
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion"
 
 import homeLogo from "./../assets/home.svg"
 import menuLogo from "./../assets/menu.svg"
@@ -10,9 +10,13 @@ import inventoryLogo from "./../assets/inventory.svg"
 import aboutusLogo from "./../assets/about_us.svg"
 import rightArrow from "./../assets/right_arrow.svg"
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 
 export function Sidebar() {
+    const [menuActive, setMenuActive] = useState(false)
+    const [inventoryActive, setInventoryActive] = useState(false)
+
     return (
         <ul css={css`
             list-style-type: none;
@@ -54,48 +58,68 @@ export function Sidebar() {
                     width: 8px;
                 }
             }
-        `}>
-            <Link to="/" style={{ textDecoration: 'none' }}>
-                <li>
-                    <section>
-                        <img src={homeLogo} alt="Home" />
-                        <p>Home</p>
-                    </section>
-                </li>
-            </Link>
-            <Link to="/menu" style={{ textDecoration: 'none' }}>
-                <li>
-                    <section>
-                        <img src={menuLogo} alt="Menu" />
-                        <p>Menu</p>
-                    </section>
-                    <motion.img initial={{ rotateZ: 90 }} src={rightArrow} alt="Expand" />
-                </li>
-            </Link>
-            <li>
-                <section>
-                    <img src={cartLogo} alt="Order & Invoice" />
-                    <p>Order & Invoice</p>
-                </section>
-                <motion.img initial={{ rotateZ: 90 }} src={rightArrow} alt="Expand" />
-            </li>
-            <Link to="/inventory" style={{ textDecoration: 'none' }}>
-                <li>
-                    <section>
-                        <img src={inventoryLogo} alt="Inventory" />
-                        <p>Inventory</p>
-                    </section>
-                    <motion.img initial={{ rotateZ: 90 }} src={rightArrow} alt="Expand" />
-                </li>
-            </Link>
-            <Link to="/about" style={{ textDecoration: 'none' }}>
-                <li>
-                    <section>
-                        <img src={aboutusLogo} alt="About us" />
-                        <p>About us</p>
-                    </section>
-                </li>
-            </Link>
+            `}>
+            <LayoutGroup>
+                <Link key="Home" to="/" style={{ textDecoration: 'none' }}>
+                    <motion.li>
+                        <motion.section>
+                            <motion.img src={homeLogo} alt="Home" />
+                            <motion.p>Home</motion.p>
+                        </motion.section>
+                    </motion.li>
+                </Link>
+                <Link key="Menu" to="/menu" style={{ textDecoration: 'none' }}>
+                    <motion.li layout>
+                        <motion.section>
+                            <motion.img src={menuLogo} alt="Menu" />
+                            <motion.p>Menu</motion.p>
+                        </motion.section>
+                        <motion.img whileHover={{ scale: 1.3 }} initial={{ rotateZ: 90 }} animate={{ rotateZ: menuActive ? 270 : 90 }} src={rightArrow} alt="Expand" onClick={() => setMenuActive(!menuActive)} />
+                    </motion.li>
+                </Link>
+                <AnimatePresence>
+                    {menuActive && (
+                        <>
+                            <motion.li layout initial={{ opacity: 0, backgroundColor: backgroundMenuItem }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                                <section>
+                                    <p>{">"} Cookies</p>
+                                </section>
+                            </motion.li>
+                            <motion.li layout initial={{ opacity: 0, backgroundColor: backgroundMenuItem }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                                <section>
+                                    <p>{">"} Beverages</p>
+                                </section>
+                            </motion.li>
+                        </>
+                    )}
+                </AnimatePresence>
+                <Link key={"Order & Invoice"} to="/order_and_invoice" style={{ textDecoration: 'none' }}>
+                    <motion.li layout>
+                        <section>
+                            <img src={cartLogo} alt={"Order & Invoice"} />
+                            <p>{"Order & Invoice"}</p>
+                        </section>
+                        <motion.img whileHover={{ scale: 1.3 }} initial={{ rotateZ: 90 }} src={rightArrow} alt="Expand" />
+                    </motion.li>
+                </Link>
+                <Link key="Inventory" to="/inventory" style={{ textDecoration: 'none' }}>
+                    <motion.li layout>
+                        <section>
+                            <img src={inventoryLogo} alt="Inventory" />
+                            <p>Inventory</p>
+                        </section>
+                        <motion.img whileHover={{ scale: 1.3 }} initial={{ rotateZ: 90 }} animate={{ rotateZ: inventoryActive ? 270 : 90 }} src={rightArrow} alt="Expand" onClick={() => setInventoryActive(!inventoryActive)} />
+                    </motion.li>
+                </Link>
+                <Link key="About" to="/about" style={{ textDecoration: 'none' }}>
+                    <motion.li layout>
+                        <section>
+                            <img src={aboutusLogo} alt="About us" />
+                            <p>About us</p>
+                        </section>
+                    </motion.li>
+                </Link>
+            </LayoutGroup>
         </ul>
     )
 }
